396cba050b89f9180891ab9dd4680c282169b47d
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-18 14:33:16 -0400

    Archiving Flatiron Code.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  
+  #prints out a numbered list of each dwarf given in an array of dwarves.
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  #modifies given array to capitalize the first letter of each call and add an exclamation mark at the end
+  planeteer_calls.map {|planateer_call| planateer_call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(long_calls)
+  #iterate through array of calls and returns true if any call is longer than 4 characters. otherwise returns false
+  long_calls.any? {|long_call| long_call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  #iterates through an array of cheeses, returning the first cheese that is included in the provided list of foods
+  cheese_types.each {|cheese| return cheese if foods.include?(cheese)}
+  # if there is no cheese in the list of foods, return nil
+  return nil
+
 end
